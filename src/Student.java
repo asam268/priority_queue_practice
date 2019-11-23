0c1f11e34bce7bb5bc55085d1dce6e835b4f5182
Asa Marshall <asam268@gmail.com>
2019-11-23 15:13:14 -0500

    Implementing Priority Queue

---
+++ src/Student.java
@@ -0,0 +1,45 @@
+public class Student implements Comparable<Student>{
+
+    private int id;
+    private String name;
+    private double cgpa;
+
+    public Student(int id, String name, double cgpa){
+        this.id = id;
+        this.name = name;
+        this.cgpa = cgpa;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getCgpa() {
+        return cgpa;
+    }
+
+    @Override
+    public int compareTo(Student o) {
+        if(this.cgpa == o.cgpa){
+            if(this.name.equalsIgnoreCase(o.name))
+                return this.id - o.id;
+            else
+                return this.name.compareTo(o.name);
+        }
+        else{
+            if(this.cgpa > o.cgpa)
+                return -1;
+            else
+                return 1;
+        }
+    }
+
+    @Override
+    public String toString(){
+        return this.name + " " + cgpa + " " + id;
+    }
+}
